f473846c96eb08d6a7836841a09e7a7bf3643efe
Aditya Mishra <100842818+aditya2131@users.noreply.github.com>
2024-10-24 11:45:39 +0530

    Updated app.js and other files for deployment

--- app.js
+++ app.js
@@ -1,58 +1,51 @@
-   //jshint esversion:6
+//jshint esversion:6
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
-   const express = require("express");
-   const bodyParser = require("body-parser");
-   const date = require(__dirname + "/date.js");
+const app = express();
 
-   const app = express();
+const items = ["Buy Food", "Cook Food", "Eat Food"];
+const workItems = [];
 
-   const items = [ "Buy Food", "Cook Food", "Eat Food"];
-   const workItems = [];
+app.set('view engine', 'ejs');
 
-   app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-   app.use(bodyParser.urlencoded({extended: true}));
-   app.use(express.static("public"));
+app.get("/", function(req, res) {
+  const day = date.getDate();
+  res.render("list", {listTitle: day, newListItems: items});
+});
 
-   app.get("/", function(req, res) {
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+  
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
 
-const day = date.getDate();
+app.get("/work", function(req, res) {
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
-     res.render("list", {
-       listTitle: day, newListItems: items
-     });
-
-   });
-
-    // home route
-   app.post("/", function(req, res){
-     const item = req.body.newItem;
-     if (req.body.list === "Work") {
-       workItems.push(item);
-       res.redirect("/work");
-     } else {
-       items.push(item);
-        res.redirect("/");
-     }
-
-
-   });
-
-   app.get("/work", function(req, res){
-     res.render("list", {listTitle: "Work List", newListItems: workItems});
-   });
-
-app.post("/work", function(req, res){
+app.post("/work", function(req, res) {
   const item = req.body.newItem;
   workItems.push(item);
   res.redirect("/work");
 });
 
-//delete route
+// delete route
 app.post("/delete", function(req, res) {
   const index = req.body.index;
-  if (req.body.list === "Work") {
+  
+  if (req.body.list === "Work List") {
     workItems.splice(index, 1);
     res.redirect("/work");
   } else {
@@ -61,7 +54,7 @@ app.post("/delete", function(req, res) {
   }
 });
 
-//edit route
+// edit route
 app.get("/edit", function(req, res) {
   const index = req.query.index;
   const list = req.query.list;
@@ -83,6 +76,7 @@ app.get("/edit", function(req, res) {
 app.post("/edit", function(req, res) {
   const index = req.body.index;
   const newItem = req.body.newItem;
+  
   if (req.body.list === "Work List") {
     workItems[index] = newItem;
     res.redirect("/work");
@@ -92,11 +86,11 @@ app.post("/edit", function(req, res) {
   }
 });
 
-  //about rote
-app.get("/about", function(req, res){
+// about route
+app.get("/about", function(req, res) {
   res.render("about");
 });
 
 app.listen(3000, function() {
-  console.log("Brain the size of earth, well starting your server on port 3000. Shut up Aditya!");
+  console.log("Server started on port 3000");
 });
